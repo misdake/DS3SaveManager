c80237718d32f4c12f556283076da46bda6c1c8b
misdake <misdake@gmail.com>
2017-12-11 16:01:17 +0800

    add windows hotkey listener.

--- src/main/java/com/rs/app/ds3/savemanager/Main.java
+++ src/main/java/com/rs/app/ds3/savemanager/Main.java
@@ -1,7 +1,26 @@
 package com.rs.app.ds3.savemanager;
 
+import com.melloware.jintellitype.JIntellitype;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("!");
+
+        JIntellitype.getInstance().registerHotKey(1, "WIN+numpad1");
+
+        JIntellitype.getInstance().addHotKeyListener(bindingIndex -> {
+            switch (bindingIndex) {
+                case 1:
+                    System.out.println("WIN+numpad1 hotkey pressed");
+                    break;
+            }
+        });
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
